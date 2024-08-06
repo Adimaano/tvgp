88766458eddb1ee07dd00e0330dd62ca09b90094
Clark <clark.rabanillo@hotmail.com>
2024-08-07 00:02:49 +0200

    test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,31 @@
-const app = require('express')();
-const fetch = require('node-fetch');
+const app = require("express")();
+const fetch = require("node-fetch");
 
-const coreAPI = 'http://core:8080/data'; // this is the exposed API from core container
+const coreAPI = "http://core:8080/data"; // this is the exposed API from core container
 
 let dataSet;
 
 async function fetchData() {
   const response = await fetch(coreAPI);
-  await response.json().then(data => dataSet = data);
+  await response.json().then((data) => (dataSet = data));
   return response;
 }
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) =>
+  res.json({
+    message:
+      'The dockerized frontend container is running and says "Hello World! test test test',
+  })
+);
+
+app.get("/", (req, res) => {
   fetchData().then(() => res.send(dataSet));
 });
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log('app is running on http://localhost:5001 after port forwarding from container to host'));
+app.listen(port, () =>
+  console.log(
+    "app is running on http://localhost:5001 after port forwarding from container to host"
+  )
+);
