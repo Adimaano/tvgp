406f772931fa0b0d113cddce96b9f3c816b15cbf
Adrian Carl Dimaano <ai22m033@technikum-wien.at>
2025-01-04 06:03:50 +0100

    simplified the async function call. python backend is already dynamic. handling the response from python flask already requires promises in nodefetching

--- frontend/pages/landing/index.ts
+++ frontend/pages/landing/index.ts
@@ -1,28 +1,24 @@
 const app = require('express')();
 const nodefetch = require('node-fetch');
 
-const coreAPI = 'http://core:5000/data'; // this is the exposed API from core container
+const coreAPI = 'http://core:8080/data'; // this is the exposed API from core container
 
 let dataSet: any;
 
 
 app.get('/', (req: any, res: any) => {
-  const response = nodefetch(coreAPI);
-
-  res.write('Hello from landing page!\n');
-  res.end("response.text()");
-  // fetchData()
-  //   .then( () =>
-  //     res.send(dataSet)
-  //   );
+  nodefetch(coreAPI)
+  .then( () =>  {
+    res.status(200)
+    res.write('Hello from the landing page')
+    res.end(dataSet)
+  })
+  .catch( () => {
+    res.status(500)
+    res.write('Error fetching data')
+    res.end(dataSet)
+  });
 });
-
-
-// async function fetchData() {
-//   const response = await nodefetch(coreAPI);
-//   await response.json().then((data: any) => dataSet = data);
-//   return response;
-// }
 
 const port = process.env.PORT || 8080;
 
